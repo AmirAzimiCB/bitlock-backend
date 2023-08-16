0b1224183d59bb2252ecfd160e959edc088766dc
Yasir Khattak <yasirkhattak46@gmail.com>
2023-08-16 15:57:23 +0500

    Loan

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,170 +8,177 @@ import WalletGroup from "../models/WalletGroup.js";
 import Bank from "../models/Bank.js";
 import {getUser} from "../utils/utils.js";
 import LoanPayments from "../models/LoanPayments.js";
+
 dotenv.config();
 
-export const checkLogin = async(req, res) => {
-  let user = await getUser(req.user.id);
-  return res.status(200).json({status:"Success", user}).end();
+export const checkLogin = async (req, res) => {
+    let user = await getUser(req.user.id);
+    return res.status(200).json({status: "Success", user}).end();
 }
 export const updatePersonalInfo = async (req, res) => {
-  let gid= [];
-  if(req.files){
-    for(let i=0; i<req.files.length; i++){
-      gid[i]=req.files[i].filename;
+    let gid = [];
+    if (req.files) {
+        for (let i = 0; i < req.files.length; i++) {
+            gid[i] = req.files[i].filename;
+        }
     }
-  }
-  if(req.body?.removed_images != undefined){
-    req.body.removed_images.forEach((item)=>{
-      fs.rmSync("uploads/"+item, {
-        force: true,
-      });
-    });
-  }
-  try {
-    if(gid.length>0){
-      if(req.body?.old_images != undefined){
-        gid.push(...req.body.old_images);
-      }
-      let government_issued_identification = gid.join();
-      await User.updateOne({ _id: req.params.id }, {
-        government_issued_identification: government_issued_identification,
-        ...req.body,
-      });
-    } else {
-      if(req.body?.old_images != undefined){
-        gid.push(...req.body.old_images);
-      }
-      let government_issued_identification = gid.join();
-      await User.updateOne({ _id: req.params.id }, {
-        government_issued_identification: government_issued_identification,
-        ...req.body,
-      });
+    if (req.body?.removed_images != undefined) {
+        req.body.removed_images.forEach((item) => {
+            fs.rmSync("uploads/" + item, {
+                force: true,
+            });
+        });
     }
-    let user = await User.findOne({ _id: req.params.id });
-    return res.status(200).json({status:"Success", user:user}).end();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err).end();
-  }
+    try {
+        if (gid.length > 0) {
+            if (req.body?.old_images != undefined) {
+                gid.push(...req.body.old_images);
+            }
+            let government_issued_identification = gid.join();
+            await User.updateOne({_id: req.params.id}, {
+                government_issued_identification: government_issued_identification,
+                ...req.body,
+            });
+        } else {
+            if (req.body?.old_images != undefined) {
+                gid.push(...req.body.old_images);
+            }
+            let government_issued_identification = gid.join();
+            await User.updateOne({_id: req.params.id}, {
+                government_issued_identification: government_issued_identification,
+                ...req.body,
+            });
+        }
+        let user = await User.findOne({_id: req.params.id});
+        return res.status(200).json({status: "Success", user: user}).end();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err).end();
+    }
 };
 export const updateBankingInfo = async (req, res) => {
-  try {
-    let user = await User.findOne({ _id: req.params.id });
-    if(req.body?.id != undefined){
-        await Bank.updateOne({ _id:req.body.id }, {
-          ...req.body,
-        });
-    } else {
-      let bank = new Bank({...req.body});
-      bank.save();
-      user.banks.push(bank._id);
-      user.save();
+    try {
+        let user = await User.findOne({_id: req.params.id});
+        if (req.body?.id != undefined) {
+            await Bank.updateOne({_id: req.body.id}, {
+                ...req.body,
+            });
+        } else {
+            let bank = new Bank({...req.body});
+            bank.save();
+            user.banks.push(bank._id);
+            user.save();
+        }
+        user = await getUser(user._id);
+        return res.status(200).json({status: "Success", result: "Saved", user}).end();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err).end();
     }
-    user = await getUser(user._id);
-    return res.status(200).json({status:"Success", result:"Saved", user}).end();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err).end();
-  }
 };
 export const applyLoan = async (req, res) => {
-  console.log(req.body);
-  try {
-    let user = await User.findOne({ _id: req.params.id });
-    const loan = new Loan({
-      ...req.body,
-    });
-    loan.user = user;
-    loan.save();
-    user.loans.push(loan._id)
-    user.save();
-    return res.status(200).json("Success").end();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err).end();
-  }
+    console.log(req.body);
+    try {
+        let user = await User.findOne({_id: req.params.id});
+        const loan = new Loan({
+            ...req.body,
+        });
+        loan.user = user;
+        loan.save();
+        user.loans.push(loan._id)
+        user.save();
+        return res.status(200).json("Success").end();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err).end();
+    }
 }
 export const uploadIdentityFiles = async (req, res) => {
-  let gid= [];
-  if(req.files){
-    for(let i=0; i<req.files.length; i++){
-      gid[i]=req.files[i].filename;
+    let gid = [];
+    if (req.files) {
+        for (let i = 0; i < req.files.length; i++) {
+            gid[i] = req.files[i].filename;
+        }
     }
-  }
-  if(gid.length>0) {
-    let fileNames = gid.join();
-    try {
-      await User.updateOne({_id: req.params.id}, {
-        identity_files: fileNames,
-      });
-      let user = await User.findOne({_id: req.params.id});
-      return res.status(200).json({status: "Success", user: user}).end();
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err).end();
+    if (gid.length > 0) {
+        let fileNames = gid.join();
+        try {
+            await User.updateOne({_id: req.params.id}, {
+                identity_files: fileNames,
+            });
+            let user = await User.findOne({_id: req.params.id});
+            return res.status(200).json({status: "Success", user: user}).end();
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err).end();
+        }
+    } else {
+        let user = await User.findOne({_id: req.params.id});
+        return res.status(200).json({status: "Success", user: user}).end();
     }
-  } else{
-    let user = await User.findOne({_id: req.params.id});
-    return res.status(200).json({status: "Success", user: user}).end();
-  }
 };
 export const getMyLoans = async (req, res) => {
-  let loans = await Loan.find({user:req.params.id, approved: 'Approved'}).populate('payments');
-  return res.status(200).json({status:"Success", loans}).end();
+    let loans = await Loan.find({user: req.params.id, approved: 'Approved'}).populate('payments');
+    return res.status(200).json({status: "Success", loans}).end();
 }
 export const getMyPendingLoans = async (req, res) => {
-  let loans = await Loan.find({user:req.params.id, approved: null}).populate('payments');
-  return res.status(200).json({status:"Success", loans}).end();
+    let loans = await Loan.find({user: req.params.id, approved: null}).populate('payments');
+    return res.status(200).json({status: "Success", loans}).end();
 }
 export const saveWallet = async (req, res) => {
-  try {
-    let user = await User.findOne({ _id: req.params.id });
-    let walletGroup = null
-    if(req.body?.id != undefined) {
-      walletGroup = await WalletGroup.findOne({ _id: req.body.id });
-      walletGroup.group_name = req.body.group_name;
-      walletGroup.wallets = [];
-      await Wallet.deleteMany({wallet_group:req.body.id});
-    } else {
-      walletGroup = new WalletGroup(
-          {
-            group_name:req.body.group_name,
-            wallets:[],
-            user
-          }
-      );
-      user.wallet_groups.push(walletGroup._id)
-      user.save();
+    try {
+        let user = await User.findOne({_id: req.params.id});
+        let walletGroup = null
+        if (req.body?.id != undefined) {
+            walletGroup = await WalletGroup.findOne({_id: req.body.id});
+            walletGroup.group_name = req.body.group_name;
+            walletGroup.wallets = [];
+            await Wallet.deleteMany({wallet_group: req.body.id});
+        } else {
+            walletGroup = new WalletGroup(
+                {
+                    group_name: req.body.group_name,
+                    wallets: [],
+                    user
+                }
+            );
+            user.wallet_groups.push(walletGroup._id)
+            user.save();
+        }
+        for (let i = 0; i < req.body.address.length; i++) {
+            const wallet = new Wallet({
+                address: req.body.address[i],
+                percentage: req.body.percentage[i],
+                wallet_group: walletGroup._id
+            });
+            wallet.save();
+            walletGroup.wallets.push(wallet._id)
+        }
+        walletGroup.save();
+        user = await getUser(user._id);
+        return res.status(200).json({status: "Success", result: "Saved", user}).end();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err).end();
     }
-    for(let i=0; i<req.body.address.length;i++) {
-      const wallet = new Wallet({
-        address:req.body.address[i],
-        percentage:req.body.percentage[i],
-        wallet_group:walletGroup._id
-      });
-      wallet.save();
-      walletGroup.wallets.push(wallet._id)
-    }
-    walletGroup.save();
-    user = await getUser(user._id);
-    return res.status(200).json({status:"Success", result:"Saved", user}).end();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err).end();
-  }
 }
 export const getMyWallets = async (req, res) => {
-  let walletGroups = await WalletGroup.find({user:req.params.id}).populate('wallets');
-  return res.status(200).json({status:"Success", walletGroups}).end();
+    let walletGroups = await WalletGroup.find({user: req.params.id}).populate('wallets');
+    return res.status(200).json({status: "Success", walletGroups}).end();
 }
 export const getAdminWallet = async (req, res) => {
-  let wallet = await AdminWallet.findOne()
-  return res.status(200).json({status:"Success", wallet}).end();
+    let wallet = await AdminWallet.findOne()
+    return res.status(200).json({status: "Success", wallet}).end();
 }
 export const savePaymentPaid = async (req, res) => {
-  await LoanPayments.updateOne({_id:req.params.id},{
-    paid:req.body.paid,
-  });
-  return res.status(200).json({status:"Success", result:"Saved"}).end();
+    await LoanPayments.updateOne({_id: req.params.id}, {
+        paid: req.body.paid,
+    });
+    if (req.body?._id){
+        await Loan.updateOne({_id:req.body?._id},{
+            collateral_amount:req?.body?.collateral_amount
+        })
+    }
+    console.log("loan_id", req.body?._id)
+    return res.status(200).json({status: "Success", result: "Saved"}).end();
 }
--- models/Loan.js
+++ models/Loan.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const Loan = new mongoose.Schema({
         borrow_amount: {type: String, required: true},
+        collateral_amount: {type: String, required: true},
         term_length: {type: String},
         loan_type: {type: String},
         category: {type: String},
@@ -11,7 +12,7 @@ const Loan = new mongoose.Schema({
         wallet_group: {type: mongoose.Types.ObjectId, ref: "WalletGroup"},
         bank: {type: mongoose.Types.ObjectId, ref: "Bank"},
         payments: [{type: mongoose.Types.ObjectId, ref: "LoanPayments"}],
-        admin_wallet: {type:String, default: null}
+        admin_wallet: {type: String, default: null}
     },
     {timestamps: true}
 )
